81b66b303f14f57aa8b9609bd71a51779b8ef52f
127307 <127307@LT101640.cts.com>
2019-01-31 18:24:45 +0530

    first commit

---
+++ testgit2/src/main/java/com/mm/testgit2/App.java
@@ -0,0 +1,13 @@
+package com.mm.testgit2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
